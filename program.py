f0e53f3a40ce8facd89af7eebdc77ed431a3d921
Andrew Monnin <acmonnin@gmail.com>
2019-09-29 12:08:27 -0400

    My first commit

---
+++ program.py
@@ -0,0 +1,7 @@
+print('-------')
+print("Hello Pythonic world")
+print()
+name = input('Hello, what is your name? ')
+greeting = 'Hello '+name+' welcome to python!'
+print(greeting)
+print("howa about this, ",name)
